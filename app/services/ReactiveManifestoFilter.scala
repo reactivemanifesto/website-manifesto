04eb2f0c371b6e7d6db02f638207f4ff0e60d6cc
Justin Pihony <justin.pihony@gmail.com>
2020-10-28 17:09:24 -0400

    final tweaks

--- app/services/ReactiveManifestoFilter.scala
+++ app/services/ReactiveManifestoFilter.scala
@@ -5,7 +5,10 @@ import play.api.mvc.{EssentialAction, EssentialFilter, Results}
 
 class ReactiveManifestoFilter extends EssentialFilter {
   override def apply(next: EssentialAction): EssentialAction = EssentialAction { rh =>
-    if (!rh.secure) {
+    val isHealthCheck = rh.path == "/health"
+    if(isHealthCheck){
+      next(rh)
+    } else if (!rh.secure) {
       Accumulator.done(Results.MovedPermanently(s"https://${rh.host}${rh.uri}"))
     } else if (rh.host == "reactivemanifesto.org") {
       Accumulator.done(Results.MovedPermanently(s"https://www.reactivemanifesto.org${rh.uri}"))
