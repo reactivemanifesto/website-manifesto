2b21a015146c7e975092eb5882367fc00068f21f
Justin Pihony <justin.pihony@gmail.com>
2023-11-10 14:16:49 -0500

    fix compiler

--- app/services/ReactiveManifestoFilter.scala
+++ app/services/ReactiveManifestoFilter.scala
@@ -7,7 +7,7 @@ class ReactiveManifestoFilter extends EssentialFilter {
   override def apply(next: EssentialAction): EssentialAction = EssentialAction { rh =>
     val isHealthCheck = rh.path == "/health"
     val requestHttps: Boolean = rh.headers.get("X-Forwarded-Proto").exists(_.equalsIgnoreCase("https"))
-    if (isHealthCheck || requestHttps || rh.secure || config.getOptional[Boolean]("session.secure").filter(identity).isEmpty) {
+    if (isHealthCheck || requestHttps || rh.secure) {
       next(rh)
     } else if (rh.host == "reactivemanifesto.org") {
       Accumulator.done(Results.MovedPermanently(s"https://www.reactivemanifesto.org${rh.uri}"))
